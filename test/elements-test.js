431240b90f7d65f8126b4376b791b0dbac069d12
frinsh <daniel.haag@expressen.se>
2018-02-12 14:41:02 +0100

    Set bottom and top to 99999 as default

--- test/elements-test.js
+++ test/elements-test.js
@@ -358,8 +358,8 @@ describe("elements", () => {
         left: 0,
         right: 0,
         width: 0,
-        bottom: 0,
-        height: 0
+        bottom: 99999,
+        height: 99999
       });
     });
 
