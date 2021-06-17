ce9f7ba28b0b52dba99cec09a5b06bf85068a945
Jonas Waldén (Exp) <jonas.walden@expressen.se>
2021-06-17 23:48:16 +0200

    moved request containment to browser

--- test/examples/infinite-scroll/test.js
+++ test/examples/infinite-scroll/test.js
@@ -3,42 +3,31 @@
 const { Browser, Painter, Resources } = require('../../../index.js');
 const { strict: assert } = require('assert');
 const app = require('./app.js');
-const nock = require('nock');
 const reset = require('../helpers/reset.js');
 const supertest = require('supertest');
 
 Feature('infinite scroll', () => {
 	before(reset);
 
-	let page, painter, resources, dom;
+	let painter, resources, dom;
 	before('load page', async () => {
 		const agent = supertest.agent(app);
-		const browser = Browser(agent);
-		page = browser.newPage();
 		painter = Painter();
 		resources = new Resources();
-		dom = await page.navigateTo('/', {}, {
-			beforeParse (window) {
-				painter.init(window);
-			}
-		});
-
-		nock(dom.window.location.origin)
-			.get(/.*/)
-			.reply(function forwardToApp (uri, body, callback) {
-				agent[this.req.method.toLowerCase()](uri)
-					.set(this.req.headers)
-					.then(res => callback(null, [ res.status, res.text, res.headers ]))
-					.catch(callback);
-			})
-			.persist();
+		dom = await Browser(agent)
+			.newPage()
+			.navigateTo('/', {}, {
+				beforeParse (window) {
+					painter.init(window);
+				}
+			});
 	});
 
 	let articles;
 	Given('one article', () => {
 		articles = dom.window.document.getElementsByTagName('article');
 		assert.equal(articles.length, 1);
-		painter.paint(articles[0], { y: 0, height: dom.window.innerHeight * 2 });
+		painter.paint('article', { height: dom.window.innerHeight * 2 });
 	});
 
 	When('scripts are executed', async () => {
@@ -62,7 +51,7 @@ Feature('infinite scroll', () => {
 		await pendingInsertion;
 		assert.equal(articles.length, 2);
 		const { bottom: previousArticleBottom } = articles[0].getBoundingClientRect();
-		painter.paint(articles[1], { y: previousArticleBottom, height: dom.window.innerHeight * 2 });
+		painter.paint(articles[1], { y: previousArticleBottom });
 	});
 
 	When('document is scrolled towards the end of the second article', () => {
