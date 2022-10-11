0a20a9356a52787fe76f2b2ae2c824de5f51c76c
Pål Edman <pal.edman@gmail.com>
2022-10-11 11:30:46 +0200

    implement custom element
    - fix submit from click emitting PointerEvent picked up by form element and submitted
    - new option for regexp override of matchMedia mediaQuery
    - add setProperty to CSSStyleDeclaration
    - submitter property to SubmitEvent
    - fix target property of Event
    - fix currentTarget property of Event

---
+++ test/custom-elements-test.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const app = require("../app/app");
+const Browser = require("..");
+const DOMException = require("domexception");
+
+describe("Custom elements", () => {
+  let browser;
+  before(() => {
+    browser = new Browser(app, { matchMedia: /.*/, console });
+  });
+
+  it("window.customElements.define adds custom element behavior", async () => {
+    const page = await browser.navigateTo("/custom-element.html");
+    page.window.styleMedia = { type: "only all" };
+    page.runScripts();
+
+    expect(page.window.customElements.get("waiting-button")).to.be.a("function");
+
+    const elm = page.document.getElementsByTagName("waiting-button")[0];
+    expect(elm.getAttribute("style")).to.equal("--spinner-delay: 100;");
+
+    const button = page.document.getElementById("clickme");
+    button.click();
+    expect(button.classList.contains("btn--waiting"), "click btn--waiting class").to.be.true;
+  });
+
+  it("window.customElements.define throws if same name is added twice", async () => {
+    const page = await browser.navigateTo("/custom-element.html");
+    page.window.customElements.define("me-button", class Me {});
+    expect(() => {
+      page.window.customElements.define("me-button", class Me {});
+    }).to.throw(DOMException, "Failed to execute 'define' on 'CustomElementRegistry': the name \"me-button\" has already been used with this registry");
+  });
+});
