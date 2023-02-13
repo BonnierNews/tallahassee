0d44f4d1d2c1167bbaaf98a9b4dc0c51801abefb
Christian Andolf <christian.andolf@bonniernews.se>
2023-02-13 16:09:58 +0100

    replaced last prototype stuff with classes

--- index.js
+++ index.js
@@ -5,48 +5,50 @@ import WebPage from "./lib/WebPage.js";
 
 const kOrigin = Symbol.for("origin");
 
-export default function Tallahassee(...args) {
-  let origin, options;
-  if (typeof args[0] === "object") {
-    options = args[0];
-  } else {
-    origin = args[0];
-    options = args[1] || {};
-  }
-  if (!(this instanceof Tallahassee)) return new Tallahassee(origin, options);
-  this[kOrigin] = origin;
-  this.jar = new CookieJar();
-  this.options = options;
-}
-
-Tallahassee.prototype.navigateTo = function navigateTo(uri, headers = {}, statusCode = 200) {
-  const webPage = this._getWebPage(headers);
-  return webPage.navigateTo(uri, webPage.isOriginUrl(uri) && webPage.originRequestHeaders, statusCode);
-};
-
-Tallahassee.prototype.load = function load(markup) {
-  const webPage = this._getWebPage();
-  const resp = {
-    text: () => markup,
-    url: `${webPage.protocol}//${webPage.originHost || "127.0.0.1"}`,
-  };
-  return webPage.load(resp);
-};
-
-Tallahassee.prototype._getWebPage = function getWebPage(headers) {
-  const requestHeaders = {
-    ...normalizeHeaders(this.options.headers),
-    ...normalizeHeaders(headers),
-  };
-
-  const setCookieHeader = requestHeaders["set-cookie"];
-  if (setCookieHeader) {
-    for (const cookieStr of Array.isArray(setCookieHeader) ? setCookieHeader : [ setCookieHeader ]) {
-      const cookie = new Cookie(cookieStr);
-      this.jar.setCookie(cookie.toString());
+export default class Tallahassee {
+  constructor(...args) {
+    let origin, options;
+    if (typeof args[0] === "object") {
+      options = args[0];
+    } else {
+      origin = args[0];
+      options = args[1] || {};
     }
-    delete requestHeaders["set-cookie"];
+    if (!(this instanceof Tallahassee)) return new Tallahassee(origin, options);
+    this[kOrigin] = origin;
+    this.jar = new CookieJar();
+    this.options = options;
   }
 
-  return new WebPage(this[kOrigin], this.jar, requestHeaders, this.options);
-};
+  navigateTo(uri, headers = {}, statusCode = 200) {
+    const webPage = this._getWebPage(headers);
+    return webPage.navigateTo(uri, webPage.isOriginUrl(uri) && webPage.originRequestHeaders, statusCode);
+  }
+
+  load(markup) {
+    const webPage = this._getWebPage();
+    const resp = {
+      text: () => markup,
+      url: `${webPage.protocol}//${webPage.originHost || "127.0.0.1"}`,
+    };
+    return webPage.load(resp);
+  }
+
+  _getWebPage(headers) {
+    const requestHeaders = {
+      ...normalizeHeaders(this.options.headers),
+      ...normalizeHeaders(headers),
+    };
+
+    const setCookieHeader = requestHeaders["set-cookie"];
+    if (setCookieHeader) {
+      for (const cookieStr of Array.isArray(setCookieHeader) ? setCookieHeader : [ setCookieHeader ]) {
+        const cookie = new Cookie(cookieStr);
+        this.jar.setCookie(cookie.toString());
+      }
+      delete requestHeaders["set-cookie"];
+    }
+
+    return new WebPage(this[kOrigin], this.jar, requestHeaders, this.options);
+  }
+}
