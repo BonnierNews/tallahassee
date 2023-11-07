658899ee2a2afa2b7f9a3c1c7b80c5691f62eae5
Pål Edman <pal.edman@gmail.com>
2023-11-07 09:10:16 +0100

    make type declaration a module (#200)
    works for both ts/esm and ts hopefully
    
    Co-authored-by: Pål Edman <pal.edman@if.se>

--- index.d.ts
+++ index.d.ts
@@ -9,49 +9,52 @@ declare global {
     prototype: Document;
   }
   interface MutationObserver {
-    prototype: Document;
+    prototype: MutationObserver;
     new(callback: MutationCallback): MutationObserver;
   }
 }
 
-export class BrowserTab {
-  /** Cheerio context */
-  get $(): cheerio.Cheerio;
-  get jar(): CookieJar;
-  get response(): FetchResponse;
-  window: Window;
-  document: Document;
-  navigateTo(uri: string, headers?: Record<string, any>, statusCode?: number): Promise<BrowserTab>;
-  load(markup: string): BrowserTab;
-  focusIframe(element: HTMLElement, src: string): Promise<BrowserTab>;
-  runScript(script: HTMLElement): void;
-  runScripts(context?: Node): void;
-  mutated(targetNode: Node, config?: { childList?: boolean, attributes?: boolean }): Promise<any[]>;
+declare module "@expressen/tallahassee" {
+  export class BrowserTab {
+    /** Cheerio context */
+    get $(): cheerio.Cheerio;
+    get jar(): CookieJar;
+    get response(): FetchResponse;
+    get _pending(): Promise<BrowserTab>;
+    window: Window;
+    document: Document;
+    navigateTo(uri: string, headers?: Record<string, any>, statusCode?: number): Promise<BrowserTab>;
+    load(markup: string): BrowserTab;
+    focusIframe(element: HTMLElement, src: string): Promise<BrowserTab>;
+    runScript(script: HTMLElement): void;
+    runScripts(context?: Node): void;
+    mutated(targetNode: Node, config?: { childList?: boolean, attributes?: boolean }): Promise<any[]>;
+  }
+
+  export class WebPage {
+    navigateTo(uri: string, headers?: Record<string, any>, statusCode?: number): Promise<BrowserTab>;
+    load(markup: string): Promise<BrowserTab>;
+    submit(uri: string, options?: Record<string, any>): Promise<BrowserTab>;
+    fetch(uri: string, requestOptions?: Record<string, any>): Promise<FetchResponse>;
+  }
+
+  class Browser {
+    constructor(origin: any, options?: Record<string, any>);
+    constructor(options?: Record<string, any>);
+    navigateTo(uri: string, headers?: Record<string, any>, statusCode?: number): Promise<BrowserTab>;
+    load(markup: string): Promise<BrowserTab>;
+  }
+
+  interface DOMInterface {
+    Document: Document;
+    HTMLCollection: HTMLCollection;
+    IntersectionObserver: IntersectionObserver;
+    MutationObserver: MutationObserver;
+    Window: Window;
+    Storage: Storage;
+  }
+
+  export const DOM: DOMInterface;
+
+  export = Browser;
 }
-
-export class WebPage {
-  navigateTo(uri: string, headers?: Record<string, any>, statusCode?: number): Promise<BrowserTab>;
-  load(markup: string): BrowserTab;
-  submit(uri: string, options?: Record<string, any>): Promise<BrowserTab>;
-  fetch(uri: string, requestOptions?: Record<string, any>): Promise<FetchResponse>;
-}
-
-export class Browser {
-  constructor(origin: any, options?: Record<string, any>);
-  constructor(options?: Record<string, any>);
-  navigateTo(uri: string, headers?: Record<string, any>, statusCode?: number): Promise<BrowserTab>;
-  load(markup: string): WebPage;
-}
-
-interface DOMInterface {
-  Document: Document;
-  HTMLCollection: HTMLCollection;
-  IntersectionObserver: IntersectionObserver;
-  MutationObserver: MutationObserver;
-  Window: Window;
-  Storage: Storage;
-}
-
-export const DOM: DOMInterface;
-
-export default Browser;
